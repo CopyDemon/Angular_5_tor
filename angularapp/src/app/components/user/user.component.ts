b6a2a5cdd5024691679a2be30f2dc93ba9f0b6e5
sheng <kingdesign2012@163.com>
2019-03-10 00:53:01 -0800

    ok

---
+++ angularapp/src/app/components/user/user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { userInfo } from "os";
+
+@Component({
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
+  //   template: "<h2>John Doe</h2>"
+})
+export class UserComponent {}
