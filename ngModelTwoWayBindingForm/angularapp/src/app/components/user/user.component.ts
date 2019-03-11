deb81000e3d2d299f396df9d8d3dcd57a1080cac
sheng <kingdesign2012@163.com>
2019-03-11 00:19:05 -0700

    ngModel two way binding forms

---
+++ ngModelTwoWayBindingForm/angularapp/src/app/components/user/user.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from "@angular/core";
+import { User } from "../../models/User"
+// import { ViewUpdateFn } from "@angular/core/src/view";
+
+@Component({
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
+})
+export class UserComponent implements OnInit {
+  user: User;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.user = {
+      firstName: "john",
+      lastName: "doe",
+      age: 30,
+      address: {
+        street: "50 main street",
+        city: "Bosten",
+        state: "MA"
+      }
+    }
+  }
+}
+
+
