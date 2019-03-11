aaa56db69411d8b021fab6f86c8b61bee3647df9
sheng <kingdesign2012@163.com>
2019-03-10 21:16:15 -0700

    bind data pipe ngFor ngIf

---
+++ bindDataPipeNgForNgIf/angularapp/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { UserComponent } from "./components/user/user.component";
+import { ToDoList } from "./components/todoList/todoList.component";
+import { UsersComponent } from './components/users/users.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [AppComponent, UserComponent, ToDoList, UsersComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
